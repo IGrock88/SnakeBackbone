e42f2c0ae96d41b99ae4c87ec17b054afd85b3e3
IGrock88 <igrock88@gmail.com>
2018-08-05 00:18:45 +0800

    fix and minor refactoring

--- js/app.js
+++ js/app.js
@@ -1,23 +1,23 @@
-var FIELD_WIDTH = 20;
-var FIELD_HEIGHT = 20;
-var SNAKE_START_SIZE = 4;
-var SNAKE_STEP_INTERVAL = 500;
-var FOOD_UPDATE_INTERVAL = 5000;
-var FOODS_QUANTITY = 2;
-var START_X_COORD = Math.floor(FIELD_WIDTH / 2);
-var START_Y_COORD = Math.floor(FIELD_HEIGHT / 2);
-var CELLS_TYPES = {
+let FIELD_WIDTH = 20;
+let FIELD_HEIGHT = 20;
+let SNAKE_START_SIZE = 4;
+let SNAKE_STEP_INTERVAL = 500;
+let FOOD_UPDATE_INTERVAL = 5000;
+let FOODS_QUANTITY = 2;
+let START_X_COORD = Math.floor(FIELD_WIDTH / 2);
+let START_Y_COORD = Math.floor(FIELD_HEIGHT / 2);
+let CELLS_TYPES = {
     food: 'food',
     snake: 'snake'
 };
-var MOVE_DIRECTIONS = {
+let MOVE_DIRECTIONS = {
     left: 11,
     top: 22,
     right: 33,
     bottom: 44
 };
 
-var KEY_CODE = {
+let KEY_CODE = {
     left: 37,
     up: 38,
     right: 39,
@@ -83,8 +83,8 @@ $(function () {
             }.bind(this), FOOD_UPDATE_INTERVAL);
         },
         drawField: function () {
-            var root = $('#root');
-            var table = $('<table/>');
+            let root = $('#root');
+            let table = $('<table/>');
 
             for (let y = 0; y < FIELD_WIDTH; y++) {
                 let row = $('<tr/>', {
@@ -102,8 +102,8 @@ $(function () {
         },
         respawnSnake: function () {
             this.snake = new Snake();
-            for (var i = 0; i < SNAKE_START_SIZE; i++) {
-                var snakeUnit = new SnakeUnit({
+            for (let i = 0; i < SNAKE_START_SIZE; i++) {
+                let snakeUnit = new SnakeUnit({
                     coordX: START_X_COORD - i,
                     coordY: START_Y_COORD,
                 });
@@ -111,8 +111,8 @@ $(function () {
             }
         },
         moveSnake: function () {
-            var snakeHead = this.snake.at(0);
-            var newSnakeUnit = {};
+            let snakeHead = this.snake.at(0);
+            let newSnakeUnit = {};
             switch (this.snake.direction) { // создаем новый элемент змейки в зависимости от направления движения
                 case MOVE_DIRECTIONS.right: {
                     newSnakeUnit = new SnakeUnit({
@@ -154,7 +154,7 @@ $(function () {
             this.snake.unshift(newSnakeUnit); // добавляем голову
         },
         changeDirection: function (e) {
-            var key = e.keyCode;
+            let key = e.keyCode;
             if (this.snake.length === 0) {
                 return;
             }
@@ -191,17 +191,12 @@ $(function () {
             }
         },
         isBorder: function (newUnit) {
-            if ($('td').is('[data-coord="' + newUnit.get('coordX') + '-' + newUnit.get('coordY') + '"]')) {// проверка есть ли ячейка с задаными координатами
-                return false;
-            }
-            return true;
+            return !$('td').is('[data-coord="' + newUnit.get('coordX') + '-' + newUnit.get('coordY') + '"]');
         },
         isSnake: function(newUnit){
-            var snakeModels = this.snake.where({coordX: newUnit.get('coordX'), coordY: newUnit.get('coordY')});
-            if(snakeModels.length >= 1){
-                return true;
-            }
-            return false;
+            let snakeModels = this.snake.where({coordX: newUnit.get('coordX'), coordY: newUnit.get('coordY')});
+            return snakeModels.length >= 1;
+
         },
         createFood: function(){
             for(let i = 0; i < FOODS_QUANTITY; i++) {
@@ -217,14 +212,11 @@ $(function () {
             }
         },
         isFood: function(newUnit){
-            var foodModel = this.foods.where({coordX: newUnit.get('coordX'), coordY: newUnit.get('coordY')});
-            if(foodModel.length >= 1){
-                return true;
-            }
-            return false;
+            let foodModel = this.foods.where({coordX: newUnit.get('coordX'), coordY: newUnit.get('coordY')});
+            return foodModel.length >= 1;
         },
         snakeEat: function(newUnit){
-            var foodModel = this.foods.where({coordX: newUnit.get('coordX'), coordY: newUnit.get('coordY')});
+            let foodModel = this.foods.where({coordX: newUnit.get('coordX'), coordY: newUnit.get('coordY')});
             this.foods.remove(foodModel);
             this.score++;
         },
